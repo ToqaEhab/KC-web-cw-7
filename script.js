dd8501dced0685ae0f8842a1265d1cea262a8792
ToqaEhab <109874516+ToqaEhab@users.noreply.github.com>
2022-08-17 20:06:04 +0300

    تم

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+function BMI (w,h) {
+    let result = w / (h*h)
 
+    return result 
+
+}
+
+console.log(BMI(10,10))
+
+function Status (bmi) {
+
+    if ( bmi <18.5) {
+        return " لديك نقص في الوزن "
+    } else if ( bmi >=18.5 && bmi <25 ) {
+
+        return " وزنك صحي "
+
+    } else if ( bmi >=25 ) {
+        return " لديك زيادة في الوزن "
+    } 
+
+}
+
+function calculate() {
+
+    let weight = document.getElementById('weight').value 
+    let hight = document.getElementById('height').value 
+
+    let bmi = BMI(weight,height)
+    let desc = Status(bmi)
+
+    let div = document.getElementById('divTag')
+    div.innerText = bmi + " == " + desc 
+
+}
